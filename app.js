76681a325ed1f3557981908d75ca2a1bc525aae1
RAVI MAURYA <ravimaurya@QMAC0238.local>
2021-03-07 11:17:58 +0530

    make it better

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./Graphql_Schema/product_schema.js");
 const dotenv = require("dotenv");
+const path = require('path');
 
 dotenv.config();
 
@@ -12,13 +13,20 @@ const server = new ApolloServer({
   resolvers,
 });
 
+//Serve the static files from the React app
+app.use(express.static(path.join(__dirname,'build')));
+
 server.applyMiddleware({ app });
 
-app.use("/", (req, res) => {
-  res.status(200);
-  res.send("Hello");
-  res.end();
-});
+// app.use("/", (req, res) => {
+//   res.status(200);
+//   res.send("Hello");
+//   res.end();
+// });
+
+app.get('*',(req,res) =>{
+  res.sendFile(path.join(__dirname,'/build/index.html'));
+})
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
